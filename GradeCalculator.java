53c5b9c78c54cce64175d38046a53967b0677e9c
Bhavya <bhavyasidharthverma123@gmail.com>
2023-09-13 03:47:33 +0530

    removed redundantncy

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -5,9 +5,9 @@ public class GradeCalculator {
 
         Scanner sc = new Scanner(System.in);
 
-        int m, p, c, b, e, total = 0, avg;
+        int m, p, c, b, e, total, avg;
 
-        char grade = ' ';
+        char grade;
 
         String txt = "";
 
@@ -27,67 +27,64 @@ public class GradeCalculator {
         // calculating grade
         if (avg > 90) {
             grade = 'O';
-        } else if (avg >= 81 && avg < 91) {
+        } else if (avg >= 81) {
             grade = 'A';
-        } else if (avg >= 71 && avg < 81) {
+        } else if (avg >= 71) {
             grade = 'B';
-        } else if (avg >= 61 && avg < 71) {
+        } else if (avg >= 61) {
             grade = 'C';
-        } else if (avg >= 51 && avg < 61) {
+        } else if (avg >= 51) {
             grade = 'D';
-        } else if (avg >= 40 && avg < 51) {
+        } else if (avg >= 40) {
             grade = 'E';
-        } else if (avg < 40) {
+        } else {
             grade = 'F';
         }
 
         switch (grade) {
-            case 'O':
+            case 'O' -> {
                 txt = "Outstanding";
                 System.out.println("-------------------------");
                 System.out.println(txt + "\nTotal Marks: " + total + "\nAverage Percentage: " + avg + "%" + "\nGrade: " + grade);
                 System.out.println("-------------------------");
-
-                break;
-            case 'A':
+            }
+            case 'A' -> {
                 txt = "Fabulous";
                 System.out.println("-------------------------");
                 System.out.println(txt + "\nTotal Marks: " + total + "\nAverage Percentage: " + avg + "%" + "\nGrade: " + grade);
                 System.out.println("-------------------------");
-
-                break;
-            case 'B':
+            }
+            case 'B' -> {
                 txt = "Better";
                 System.out.println("-------------------------");
                 System.out.println(txt + "\nTotal Marks: " + total + "\nAverage Percentage: " + avg + "%" + "\nGrade: " + grade);
                 System.out.println("-------------------------");
-                break;
-            case 'C':
+            }
+            case 'C' -> {
                 txt = "Can do better";
                 System.out.println("-------------------------");
                 System.out.println(txt + "\nTotal Marks: " + total + "\nAverage Percentage: " + avg + "%" + "\nGrade: " + grade);
                 System.out.println("-------------------------");
-                break;
-            case 'D':
+            }
+            case 'D' -> {
                 txt = "Need to Study hard";
                 System.out.println("-------------------------");
                 System.out.println(txt + "\nTotal Marks: " + total + "\nAverage Percentage: " + avg + "%" + "\nGrade: " + grade);
                 System.out.println("-------------------------");
-                break;
-            case 'E':
+            }
+            case 'E' -> {
                 txt = "Very poor marks ";
                 System.out.println("-------------------------");
                 System.out.println(txt + "\nTotal Marks: " + total + "\nAverage Percentage: " + avg + "%" + "\nGrade: " + grade);
                 System.out.println("-------------------------");
-                break;
-            case 'F':
+            }
+            case 'F' -> {
                 txt = "You have failed this exam";
                 System.out.println("-------------------------");
                 System.out.println(txt + "\nTotal Marks: " + total + "\nAverage Percentage: " + avg + "%" + "\nGrade: " + grade);
                 System.out.println("-------------------------");
-                break;
-            default:
-                System.out.println(txt);
+            }
+            default -> System.out.println(txt);
         }
     }
 
